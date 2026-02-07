8948a8c238aaa9ba65fb70454d67e784f00b3d5a
Shuai Mu <msmummy@gmail.com>
2026-02-07 13:45:14 -0500

    Revert "[Shrink] Merge TPCC DELIVERY pieces 0-3 into single piece"
    This reverts commit ed67f84d311e57486cfb0bf0d52550e728ac2fe1.

--- src/bench/tpcc/delivery.cc
+++ src/bench/tpcc/delivery.cc
@@ -4,54 +4,59 @@
 namespace janus {
 
 void TpccProcedure::DeliveryInit(TxRequest &req) {
-  // Pieces 0-3 merged into single piece 0 (all W_ID-sharded)
-  n_pieces_all_ = 1;
+  n_pieces_all_ = 4;
 
-  output_size_[TPCC_DELIVERY_0] = 0;
+  // piece 0, Ri & W new_order
+  output_size_[TPCC_DELIVERY_0] = 2;
+  // piece 1, Ri & W order
+  output_size_[TPCC_DELIVERY_1] = 1;
+  // piece 2, Ri & W order_line
+  output_size_[TPCC_DELIVERY_2] = 1;
+  // piece 3, W customer
+  output_size_[TPCC_DELIVERY_3] = 0;
 
   p_types_[TPCC_DELIVERY_0] = TPCC_DELIVERY_0;
+  p_types_[TPCC_DELIVERY_1] = TPCC_DELIVERY_1;
+  p_types_[TPCC_DELIVERY_2] = TPCC_DELIVERY_2;
+  p_types_[TPCC_DELIVERY_3] = TPCC_DELIVERY_3;
 
   status_[TPCC_DELIVERY_0] = WAITING;
+  status_[TPCC_DELIVERY_1] = WAITING;
+  status_[TPCC_DELIVERY_2] = WAITING;
+  status_[TPCC_DELIVERY_3] = WAITING;
   CheckReady();
 }
 
 void TpccProcedure::DeliveryRetry() {
   status_[TPCC_DELIVERY_0] = WAITING;
+  status_[TPCC_DELIVERY_1] = WAITING;
+  status_[TPCC_DELIVERY_2] = WAITING;
+  status_[TPCC_DELIVERY_3] = WAITING;
   CheckReady();
 }
 
 
 void TpccWorkload::RegDelivery() {
-  // All 4 delivery pieces merged into piece 0 (all W_ID-sharded).
-  // Pieces 1-3 were: order R/W, order_line R/W, customer W.
+  // Ri & W new_order
   RegP(TPCC_DELIVERY, TPCC_DELIVERY_0,
        {TPCC_VAR_W_ID, TPCC_VAR_D_ID, TPCC_VAR_O_CARRIER_ID}, // i
-       {},  // o
+       {TPCC_VAR_O_ID},  // o
        {conf_id_t(TPCC_TB_NEW_ORDER,
                {TPCC_VAR_D_ID, TPCC_VAR_W_ID},
                {TPCC_COL_NEW_ORDER_NO_O_ID},
-               RS_NEW_ORDER),
-        conf_id_t(TPCC_TB_ORDER,
-               {TPCC_VAR_D_ID, TPCC_VAR_W_ID},
-               {TPCC_COL_ORDER_O_CARRIER_ID},
-               ROW_ORDER),
-        conf_id_t(TPCC_TB_ORDER_LINE,
-               {TPCC_VAR_D_ID, TPCC_VAR_W_ID},
-               {TPCC_COL_ORDER_LINE_OL_AMOUNT,
-                TPCC_COL_ORDER_LINE_OL_DELIVERY_D},
-               RS_ORDER_LINE),
-        conf_id_t(TPCC_TB_CUSTOMER,
-               {TPCC_VAR_D_ID, TPCC_VAR_W_ID},
-               {TPCC_COL_CUSTOMER_C_BALANCE,
-                TPCC_COL_CUSTOMER_C_DELIVERY_CNT},
-               ROW_CUSTOMER)}, // c
+               RS_NEW_ORDER)}, // c
        {TPCC_TB_NEW_ORDER, {TPCC_VAR_W_ID}}, // s
        DF_REAL,
        PROC {
-         // --- former piece 0: Ri & W new_order ---
+         // this is a little bit tricky, the first half will do most of the job,
+         // removing the row from the table, but it won't actually release the
+         // resource. And the bottom half is in charge of release the resource,
+         // including the vertex entry
+
          Log_debug("TPCC_DELIVERY, piece: %d", TPCC_DELIVERY_0);
          verify(cmd.input.size() >= 3);
          Value buf;
+         //cell_locator_t cl(TPCC_TB_NEW_ORDER, 3);
          mdb::Row *r = NULL;
          mdb::Table *tbl = tx.GetTable(TPCC_TB_NEW_ORDER);
 
@@ -73,50 +78,100 @@ void TpccWorkload::RegDelivery() {
          Value o_id(0);
          if (rs.has_next()) {
            r = rs.next();
+//      TPL_KISS_ROW(r);
            tx.ReadColumn(r, TPCC_COL_NEW_ORDER_NO_W_ID, &o_id, TXN_DEFERRED);
+           output[TPCC_VAR_O_ID] = o_id;
          } else {
-           *res = SUCCESS;
-           return;
+//      verify(0);
+//      TPL_KISS_NONE;
+           output[TPCC_VAR_O_ID] = Value((i32) -1);
          }
+         // TODO FIXME
+//    if (r) {
+//      mdb::Txn *txn = tx.mdb_txn();
+//      txn->remove_row(tbl, r);
+//    }
 
-         // --- former piece 1: Ri & W order ---
+         *res = SUCCESS;
+         return;
+       }
+  );
+
+  // Ri & W order
+  RegP(TPCC_DELIVERY, TPCC_DELIVERY_1,
+       {TPCC_VAR_W_ID, TPCC_VAR_D_ID,
+        TPCC_VAR_O_ID, TPCC_VAR_O_CARRIER_ID}, // i
+       {TPCC_VAR_C_ID}, // o
+       {conf_id_t(TPCC_TB_ORDER,
+               {TPCC_VAR_D_ID, TPCC_VAR_W_ID, TPCC_VAR_O_ID},
+               {TPCC_COL_ORDER_O_CARRIER_ID},
+               ROW_ORDER)}, // c
+       {TPCC_TB_ORDER, {TPCC_VAR_W_ID}}, // s
+       DF_NO,
+       PROC {
+         Log_debug("TPCC_DELIVERY, piece: %d", TPCC_DELIVERY_1);
+         verify(cmd.input.size() >= 4);
          mdb::Txn *txn = tx.mdb_txn_;
          mdb::MultiBlob mb(3);
+         //cell_locator_t cl(TPCC_TB_ORDER, 3);
          mb[0] = cmd.input[TPCC_VAR_D_ID].get_blob();
          mb[1] = cmd.input[TPCC_VAR_W_ID].get_blob();
-         mb[2] = o_id.get_blob();
+         mb[2] = cmd.input[TPCC_VAR_O_ID].get_blob();
+         //Log::debug("Delivery: o_d_id: %d, o_w_id: %d, o_id: %d, hash: %u", input[2].get_i32(), input[1].get_i32(), input[0].get_i32(), mdb::MultiBlob::hash()(cl.primary_key));
          auto tbl_order = txn->get_table(TPCC_TB_ORDER);
          mdb::Row *row_order = tx.Query(tbl_order, mb, ROW_ORDER);
-         Value c_id_val;
          tx.ReadColumn(row_order,
                           TPCC_COL_ORDER_O_C_ID,
-                          &c_id_val,
-                          TXN_BYPASS);
+                          &output[TPCC_VAR_C_ID],
+                          TXN_BYPASS); // read o_c_id
          tx.WriteColumn(row_order,
                            TPCC_COL_ORDER_O_CARRIER_ID,
                            cmd.input[TPCC_VAR_O_CARRIER_ID],
-                           TXN_DEFERRED);
+                           TXN_DEFERRED); // write o_carrier_id
+         return;
+       }
+  );
 
-         // --- former piece 2: Ri & W order_line ---
-         mdb::MultiBlob mbl_ol(4), mbh_ol(4);
-         mbl_ol[0] = cmd.input[TPCC_VAR_D_ID].get_blob();
-         mbh_ol[0] = cmd.input[TPCC_VAR_D_ID].get_blob();
-         mbl_ol[1] = cmd.input[TPCC_VAR_W_ID].get_blob();
-         mbh_ol[1] = cmd.input[TPCC_VAR_W_ID].get_blob();
-         mbl_ol[2] = o_id.get_blob();
-         mbh_ol[2] = o_id.get_blob();
+//   Ri & W order_line
+  RegP(TPCC_DELIVERY, TPCC_DELIVERY_2,
+       {TPCC_VAR_W_ID, TPCC_VAR_D_ID, TPCC_VAR_O_ID}, // i
+       {}, // TODO o
+       {conf_id_t(TPCC_TB_ORDER_LINE,
+               {TPCC_VAR_D_ID, TPCC_VAR_W_ID, TPCC_VAR_O_ID},
+               {TPCC_COL_ORDER_LINE_OL_AMOUNT,
+                TPCC_COL_ORDER_LINE_OL_DELIVERY_D},
+               RS_ORDER_LINE)}, // c
+       {TPCC_TB_ORDER_LINE, {TPCC_VAR_W_ID}}, // s
+       DF_NO,
+       PROC {
+         Log_debug("TPCC_DELIVERY, piece: %d", TPCC_DELIVERY_2);
+         verify(cmd.input.size() >= 3);
+         //        mdb::Txn *txn = DTxnMgr::get_sole_mgr()->get_mdb_txn(header);
+         mdb::MultiBlob mbl = mdb::MultiBlob(4);
+         mdb::MultiBlob mbh = mdb::MultiBlob(4);
+//    mdb::MultiBlob mbl(4), mbh(4);
+         mbl[0] = cmd.input[TPCC_VAR_D_ID].get_blob();
+         mbh[0] = cmd.input[TPCC_VAR_D_ID].get_blob();
+         mbl[1] = cmd.input[TPCC_VAR_W_ID].get_blob();
+         mbh[1] = cmd.input[TPCC_VAR_W_ID].get_blob();
+         mbl[2] = cmd.input[TPCC_VAR_O_ID].get_blob();
+         mbh[2] = cmd.input[TPCC_VAR_O_ID].get_blob();
          Value ol_number_low(std::numeric_limits<i32>::min()),
              ol_number_high(std::numeric_limits<i32>::max());
-         mbl_ol[3] = ol_number_low.get_blob();
-         mbh_ol[3] = ol_number_high.get_blob();
+         mbl[3] = ol_number_low.get_blob();
+         mbh[3] = ol_number_high.get_blob();
 
          mdb::ResultSet
              rs_ol = tx.QueryIn(tx.GetTable(TPCC_TB_ORDER_LINE),
-                                   mbl_ol,
-                                   mbh_ol,
+                                   mbl,
+                                   mbh,
                                    mdb::ORD_ASC,
                                    RS_ORDER_LINE);
          mdb::Row *row_ol = nullptr;
+         //                cell_locator_t cl(TPCC_TB_ORDER_LINE, 4);
+         //                cl.primary_key[0] = input[2].get_blob();
+         //                cl.primary_key[1] = input[1].get_blob();
+         //                cl.primary_key[2] = input[0].get_blob();
 
          std::vector<mdb::Row *> row_list;
          row_list.reserve(15);
@@ -124,42 +179,65 @@ void TpccWorkload::RegDelivery() {
            row_list.push_back(rs_ol.next());
          }
 
+         std::vector<mdb::column_lock_t> column_locks;
+         column_locks.reserve(2 * row_list.size());
+
          int i = 0;
          double ol_amount_buf = 0.0;
 
          while (i < row_list.size()) {
            row_ol = row_list[i++];
-           Value ol_buf(0.0);
+           Value buf(0.0);
            tx.ReadColumn(row_ol, TPCC_COL_ORDER_LINE_OL_AMOUNT,
-                            &ol_buf, TXN_DEFERRED);
-           ol_amount_buf += ol_buf.get_double();
+                            &buf, TXN_DEFERRED); // read ol_amount
+           ol_amount_buf += buf.get_double();
            tx.WriteColumn(row_ol, TPCC_COL_ORDER_LINE_OL_DELIVERY_D,
                              Value(std::to_string(time(NULL))),
                              TXN_DEFERRED);
          }
+         output[TPCC_VAR_OL_AMOUNT] = Value(ol_amount_buf);
 
-         // --- former piece 3: W customer ---
+         *res = SUCCESS;
+       }
+  );
+
+  // W customer
+  RegP(TPCC_DELIVERY, TPCC_DELIVERY_3,
+       {TPCC_VAR_W_ID, TPCC_VAR_D_ID,
+        TPCC_VAR_C_ID, TPCC_VAR_OL_AMOUNT}, // i
+       {}, // o
+       {conf_id_t(TPCC_TB_CUSTOMER,
+               {TPCC_VAR_C_ID, TPCC_VAR_D_ID, TPCC_VAR_W_ID},
+               {TPCC_COL_CUSTOMER_C_BALANCE,
+                TPCC_COL_CUSTOMER_C_DELIVERY_CNT},
+               ROW_CUSTOMER)}, // c
+       {TPCC_TB_CUSTOMER, {TPCC_VAR_W_ID}}, // s
+       DF_REAL,
+       PROC {
+         Log_debug("TPCC_DELIVERY, piece: %d", TPCC_DELIVERY_3);
+         verify(cmd.input.size() >= 4);
          mdb::Row *row_customer = NULL;
-         mdb::MultiBlob mb_cust(3);
-         mb_cust[0] = c_id_val.get_blob();
-         mb_cust[1] = cmd.input[TPCC_VAR_D_ID].get_blob();
-         mb_cust[2] = cmd.input[TPCC_VAR_W_ID].get_blob();
+         mdb::MultiBlob mb = mdb::MultiBlob(3);
+         //cell_locator_t cl(TPCC_TB_CUSTOMER, 3);
+         mb[0] = cmd.input[TPCC_VAR_C_ID].get_blob();
+         mb[1] = cmd.input[TPCC_VAR_D_ID].get_blob();
+         mb[2] = cmd.input[TPCC_VAR_W_ID].get_blob();
 
          auto tbl_customer = tx.GetTable(TPCC_TB_CUSTOMER);
-         row_customer = tx.Query(tbl_customer, mb_cust, ROW_CUSTOMER);
-         Value cust_buf = Value(0.0);
+         row_customer = tx.Query(tbl_customer, mb, ROW_CUSTOMER);
+         Value buf = Value(0.0);
          tx.ReadColumn(row_customer, TPCC_COL_CUSTOMER_C_BALANCE,
-                          &cust_buf, TXN_DEFERRED);
-         cust_buf.set_double(cust_buf.get_double() + ol_amount_buf);
+                          &buf, TXN_DEFERRED);
+         buf.set_double(buf.get_double() +
+             cmd.input[TPCC_VAR_OL_AMOUNT].get_double());
          tx.WriteColumn(row_customer, TPCC_COL_CUSTOMER_C_BALANCE,
-                           cust_buf, TXN_DEFERRED);
+                           buf, TXN_DEFERRED);
          tx.ReadColumn(row_customer, TPCC_COL_CUSTOMER_C_DELIVERY_CNT,
-                          &cust_buf, TXN_BYPASS);
-         cust_buf.set_i32(cust_buf.get_i32() + (i32) 1);
+                          &buf, TXN_BYPASS);
+         buf.set_i32(buf.get_i32() + (i32) 1);
          tx.WriteColumn(row_customer, TPCC_COL_CUSTOMER_C_DELIVERY_CNT,
-                           cust_buf, TXN_DEFERRED);
+                           buf, TXN_DEFERRED);
          *res = SUCCESS;
-         return;
        }
   );
 }
